ac3bd20a148913da8d0683e622a565295a464402
blackxwhite <josip_kopic@hotmail.co.uk>
2016-07-10 14:51:26 +0200

    Add files via upload

---
+++ ButterflyRecognition/ButterflyRecognition/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ButterflyRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.OpenImg = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 259);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OpenImg
+            // 
+            this.OpenImg.Location = new System.Drawing.Point(355, 12);
+            this.OpenImg.Name = "OpenImg";
+            this.OpenImg.Size = new System.Drawing.Size(75, 23);
+            this.OpenImg.TabIndex = 1;
+            this.OpenImg.Text = "Open image";
+            this.OpenImg.UseVisualStyleBackColor = true;
+            this.OpenImg.Click += new System.EventHandler(this.OpenImg_Click);
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(355, 41);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(236, 20);
+            this.Result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 310);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.OpenImg);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button OpenImg;
+        private System.Windows.Forms.TextBox Result;
+    }
+}
+
---
+++ ButterflyRecognition/ButterflyRecognition/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ButterflyRecognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void OpenImg_Click(object sender, EventArgs e)
+        {
+            string OpenImage;
+            OpenFileDialog FileDialog = new OpenFileDialog();
+            FileDialog.Filter = "jpg files (*.jpg) |*.jpg";
+            if(FileDialog.ShowDialog() == DialogResult.OK)
+            {
+                OpenImage = FileDialog.FileName;
+                Bitmap image = new Bitmap(OpenImage, true);
+                pictureBox1.Image = image;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            // full path of python interpreter 
+            string python = @"C:\Users\Josip\Desktop\WinPython-64bit-3.4.4.2\python-3.4.4.amd64\python.exe";
+
+            // python app to call 
+            string myPythonApp = "C:\\Users\\Josip\\Desktop\\ButterflyRecognition-master\\azureWebService.py";
+
+            // Create new process start info 
+            ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(python);
+
+            // make sure we can read the output from stdout 
+            myProcessStartInfo.UseShellExecute = false;
+            myProcessStartInfo.RedirectStandardOutput = true;
+
+            myProcessStartInfo.Arguments = myPythonApp;
+
+            Process myProcess = new Process();
+            // assign start information to the process 
+            myProcess.StartInfo = myProcessStartInfo;
+
+            // start the process 
+            myProcess.Start();
+
+            // Read the standard output of the app we called.  
+            // in order to avoid deadlock we will read output first 
+            // and then wait for process terminate: 
+            StreamReader myStreamReader = myProcess.StandardOutput;
+            string myString = myStreamReader.ReadLine();
+
+            /*if you need to read multiple lines, you might use: 
+                string myString = myStreamReader.ReadToEnd() */
+
+            // wait exit signal from the app we called and then close it. 
+            myProcess.WaitForExit();
+            myProcess.Close();
+
+            Result.Text = myString;
+        }
+    }
+}
